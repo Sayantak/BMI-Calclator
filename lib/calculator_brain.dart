0b8aeaaf2243cf7d46f1e12c491e2b3668f6e419
Sayantak <sayantakkarar@gmail.com>
2020-10-03 10:10:06 +0530

    Modified outdated formulae

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -8,23 +8,23 @@ class CalculatorBrain {
   double _bmi;
 
   String calculateBMI() {
-    _bmi = weight / pow(height / 100, 2);
+    _bmi = (weight-20) / pow(height / 100, 2);
     return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi >= 25)
+    if (_bmi >= 28)
       return 'Overweight';
-    else if (_bmi > 18.5)
+    else if (_bmi > 16.5)
       return 'Normal';
     else
       return 'Underweight';
   }
 
   String getInterpretation() {
-    if (_bmi >= 25)
+    if (_bmi >= 28)
       return 'You\'ve a higher than normal body weight. Try to exercise more!';
-    else if (_bmi > 18.5)
+    else if (_bmi > 16.5)
       return 'You\'ve a normal body weight. Good Job!';
     else
       return 'You\'ve a lower than normal body weight. You can eat a little bit more.';
